bf437c05d83455f1387c517b28b45749ea0107ea
Sanket Kalekar <kalekarsanket005@gmail.com>
2023-08-19 00:18:54 +0530

    Add files via upload

---
+++ TernaryOperatorsExample.java
@@ -0,0 +1,9 @@
+public class TernaryOperatorsExample {
+    public static void main(String args[]){
+        int marks = 65;
+        //ternary operators
+        String status =  marks>35? "pass":"fail";
+        System.out.println(status);
+
+    }
+}
