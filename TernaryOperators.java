9443f95bd2be2f15fb8d02ded2fd57e02d35d943
Sanket Kalekar <kalekarsanket005@gmail.com>
2023-08-19 00:18:05 +0530

    Add files via upload

---
+++ TernaryOperators.java
@@ -0,0 +1,8 @@
+public class TernaryOperators {
+    public static void main(String args[]){
+       int number = 5;
+       // trenary operator
+       String type = ((number%2) == 0) ? "even":"odd";
+       System.out.println(type);
+    }
+}
