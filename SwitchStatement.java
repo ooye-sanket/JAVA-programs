dd1124f2259f0e42e65cffcc5b92c4a7bb81ed58
Sanket Kalekar <kalekarsanket005@gmail.com>
2023-08-19 00:19:10 +0530

    Add files via upload

---
+++ SwitchStatement.java
@@ -0,0 +1,15 @@
+public class SwitchStatement {
+    public static void main(String args[]){
+       int number = 2;
+       switch(number){
+        case 1 : System.out.println("Samosa");
+        break;
+        case 2 : System.out.println("burgir");
+        break;
+        case 3 : System.out.println("Mango"); 
+        break;   
+        default : System.out.println("We up");
+       }
+       
+    }
+}
